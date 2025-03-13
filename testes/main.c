c98f6f4470deb6827b45894973b67e03f7a14aab
Anna Clara <159267164+annaclaralemossilva@users.noreply.github.com>
2025-03-13 09:35:08 -0300

    Add files via upload

--- testes/main.c
+++ testes/main.c
@@ -1,19 +1,37 @@
-/*Um programa que peça dois números e de um codigo para sua escolha!*/
+/*Gerenciamento Automatizado de Comercio - Iceball*/
 
 #include <stdio.h>
-
+int estoque(){
+    int cod;
+    printf ("\nDigite código de produto: ");
+            scanf (" %d", &cod);
+            
+            if(cod=101){
+                printf("Sorvete - 50 unidades");
+            }else if(cod=102){
+                printf("Acompanhamentos - 30 unidades");
+            }else if(cod=103){
+                printf("Picolés - 20 unidades");
+            }else if(cod=104){
+                printf("Modelos de Recipiente - 30 unidades");
+            }else{
+                printf("Opção inválida!\n");
+            }
+            return e;
+}
 int main(){
     char menu;
     printf ("Seja bem vindo ao Menu Principal da Sorveteria Ice Ball!\n");
-    printf ("Escolha: \n e) -Controle de Estoque\n v) -Controle de Vendas\n f) -Controle de Financias\n s) -Sair");
+    printf ("Escolha: \n e) -Controle de Estoque\n v) -Controle de Vendas\n f) -Controle de Financias\n s) -Sair\n");
     scanf ("\n%c", &menu);
     
     switch(menu){
         case 'e':
             printf ("Seja bem vindo(a) para o controle de estoque!\n");
-            int a;
-            printf ("Digite código de produto: ");
-            scanf ("%d", &a);
+            int cod;
+            printf ("\nDigite código de produto: ");
+            scanf (" %d", &cod);
+            
             break;
         case 'v':
             printf ("Seja bem vindo(a) para o controle de vendas!\n");
